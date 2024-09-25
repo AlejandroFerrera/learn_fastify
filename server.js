23a2b2eabb6631dae591e1016ae5eba35ce25544
AlejandroFerrera <aferrerah1421@gmail.com>
2024-09-25 18:10:47 -0300

    test: :white_check_mark: add basic unit test and closeWithGrace

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 "use strict";
 import { build } from "./app.js";
+import closeWithGrace from "close-with-grace";
 const opts = {};
 
 if (process.stdout.isTTY) {
@@ -20,3 +21,16 @@ app.listen({ port: 3000 }, function (err, address) {
 		process.exit(1);
 	}
 });
+
+closeWithGrace(async ({ signal, err }) => {
+	if (err) {
+		app.log.error({ err }, "Server closing with error");
+	} else {
+		app.log.info({ signal }, "Signal received, server closing");
+	}
+	await app.close();
+});
+
+// setTimeout(() => {
+// 	throw new Error('Forced error')
+// }, 2000)
